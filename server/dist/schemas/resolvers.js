25f24204dd5903905689478610085a7da2e7acb6
Johann <175734270+Jzaroli@users.noreply.github.com>
2026-01-19 18:59:57 -0800

    completed both input validation and set probability risk threshold to 0.18 based on updated model

--- server/dist/schemas/resolvers.js
+++ server/dist/schemas/resolvers.js
@@ -5,7 +5,50 @@ const resolvers = {
     },
     Mutation: {
         submitProfile: async (_parent, { input }) => {
-            // console.log(input)
+            // validates that all inputs are numbers
+            if (isNaN(input.age) || isNaN(input.creditScore) || isNaN(input.monthlyIncome) || isNaN(input.monthlyDebts) || isNaN(input.delinquencies30) || isNaN(input.delinquencies60) || isNaN(input.delinquencies90) || isNaN(input.openedLines) || isNaN(input.unsecuredUsage) || isNaN(input.unsecuredLimit)) {
+                throw new Error('Inputs must all be numbers.');
+            }
+            // validates correct age range
+            if (input.age < 18 || input.age > 120) {
+                throw new Error('Applicant must be between the ages of 18 and 120.');
+            }
+            // validates correct credit score range
+            if (input.creditScore < 300 || input.creditScore > 850) {
+                throw new Error('Applicant credit score must be between 300 and 850.');
+            }
+            // validates monthly income
+            if (input.monthlyIncome < 0 || input.monthlyIncome > 1000000) {
+                throw new Error('Applicant monthly income must be between $0 and $1,000,000');
+            }
+            // validates monthly debt
+            if (input.monthlyDebts < 0 || input.monthlyDebts > 1000000) {
+                throw new Error('Applicant monthly debt must be between $0 and $1,000,000');
+            }
+            // validates unsecured limit
+            if (input.unsecuredLimit < 0 || input.unsecuredLimit > 1000000) {
+                throw new Error('Applicant unsecured limit must be between $0 and $1,000,000');
+            }
+            // validates unsecured usage
+            if (input.unsecuredUsage < 0 || input.unsecuredUsage > 1000000) {
+                throw new Error('Applicant unsecured usage must be between $0 and $1,000,000');
+            }
+            // validates numberOfTime30_59
+            if (input.delinquencies30 < 0 || input.delinquencies30 > 30) {
+                throw new Error('Applicant number of times delinquent 30-59 days must be between 0 and 30');
+            }
+            // validates numberOfTime60_89
+            if (input.delinquencies60 < 0 || input.delinquencies60 > 30) {
+                throw new Error('Applicant number of times delinquent 60-89 days must be between 0 and 30');
+            }
+            // validates numberOfTime90
+            if (input.delinquencies90 < 0 || input.delinquencies90 > 30) {
+                throw new Error('Applicant number of times delinquent 90+ days must be between 0 and 30');
+            }
+            // validates number of open credit lines and loans
+            if (input.openedLines < 0 || input.openedLines > 50) {
+                throw new Error('Applicant number of open credit lines and loans must be between 0 and 50');
+            }
             let debtRatio_calc = input.monthlyDebts / input.monthlyIncome;
             let revolving_calc = input.unsecuredUsage / input.unsecuredLimit;
             const inference_payload = {
@@ -18,7 +61,6 @@ const resolvers = {
                 monthlyIncome: input.monthlyIncome,
                 numberOfOpenCreditLinesAndLoans: input.openedLines,
             };
-            // console.log('inference_payload', inference_payload)
             const inference_results = await getInference(inference_payload);
             console.log('inference results:', inference_results);
             if (!inference_results) {
